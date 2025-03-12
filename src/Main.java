e49953a27e4d3c6b8bd50538c4fb71e5fd18024b
itapita363 <itamar.royee@gmail.com>
2025-03-12 11:11:18 +0200

    fix formatting and file name

--- src/Main.java
+++ src/Main.java
@@ -8,29 +8,29 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        try (var lines = Files.lines(Path.of("./measurements.txt"))) {
+        try (var lines = Files.lines(Path.of("/home/itamar/projects/1brc/measurements.txt"))) {
             var reports = lines.parallel()
                                .map(stationReport -> {
                                    int delimiterIndex = stationReport.indexOf(";");
                                    return new Pair<>(stationReport.substring(0,
-                                           delimiterIndex),
-                                           Double.parseDouble(
-                                                   stationReport.substring(
-                                                           delimiterIndex + 1)));
+                                                                             delimiterIndex),
+                                                     Double.parseDouble(
+                                                             stationReport.substring(
+                                                                     delimiterIndex + 1)));
                                })
-                               .collect(Collectors.groupingByConcurrent(Pair::first,
-                                       Collectors.summarizingDouble(Pair::second)));
+                               .collect(Collectors.groupingBy(Pair::first,
+                                                              Collectors.summarizingDouble(Pair::second)));
             var messages = reports.entrySet()
                                   .stream()
                                   .parallel()
                                   .map(entry -> String.format("%s=%.1f/%.1f/%.1f, ",
-                                          entry.getKey(),
-                                          entry.getValue()
-                                               .getMin(),
-                                          entry.getValue()
-                                               .getAverage(),
-                                          entry.getValue()
-                                               .getMax()))
+                                                              entry.getKey(),
+                                                              entry.getValue()
+                                                                   .getMin(),
+                                                              entry.getValue()
+                                                                   .getAverage(),
+                                                              entry.getValue()
+                                                                   .getMax()))
                                   .toList();
             System.out.print("{");
             messages.forEach(System.out::print);
